af107d501674e3c008b139872eaa3f48ceeaea9e
ducnn.230288 <ducnn.230288@gmail.com>
2025-07-16 19:31:38 +0700

    feat(base): init source code

---
+++ src/shared/components/organisms/form/field/type.ts
@@ -0,0 +1,17 @@
+import type { TFieldForm, TForm, TFormField } from '@/shared/types';
+
+/**
+ * Represents the properties for the button component.
+ */
+type Props<T> = {
+  readonly formApi: TForm<T>;
+  readonly fieldForm: TFieldForm<T>;
+  readonly Field: TFormField<T>;
+  readonly name: keyof T;
+  readonly index: string | number;
+  readonly values?: T;
+  readonly isLabel?: boolean;
+  readonly translate: (key: string, options?: Record<string, string>) => string;
+};
+
+export default Props;
